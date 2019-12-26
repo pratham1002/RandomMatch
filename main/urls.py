c20b8763e2b3afa2182822b750a5d27c8a6eae49
pratham1002 <54350608+pratham1002@users.noreply.github.com>
2019-12-26 18:28:50 +0530

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('useraccounts.urls')),
     path('admin/', admin.site.urls),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
